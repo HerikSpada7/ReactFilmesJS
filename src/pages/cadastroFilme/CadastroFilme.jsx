48d1b85757c09ad24e3c19b4a7db3f4f2e965f58
Herik <herik.spada@aluno.senai.br>
2025-04-24 16:50:09 -0300

    :sparkless: Feat: Adicionando o footer, cadastro e a lista

--- src/pages/cadastroFilme/CadastroFilme.jsx
+++ src/pages/cadastroFilme/CadastroFilme.jsx
@@ -1,8 +1,18 @@
-import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Cadastro from "../../components/cadastro/Cadastro";
+import Lista from "../../components/lista/Lista";
 
 const CadastroFilme = () => {
     return(
-        <Header/>
+        <>
+            <Header/>
+            <main>
+                <Cadastro/>
+                <Lista/> 
+            </main>
+            <Footer/> 
+        </>
     )
 }
 
